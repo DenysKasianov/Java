f85ef14047477d8d91edc14798e5197cb9bccd22
DenysKasianov <kasyanovwork1993@gmail.com>
2023-01-29 14:17:26 -0700

    Create new package and class inside

---
+++ src/Homework1/hello.java
@@ -0,0 +1,8 @@
+package Homework1;
+
+public class hello {
+    public static void main(String[] args) {
+        String name = "Denys";
+        System.out.println("Hello " + name + "!");
+    }
+}
