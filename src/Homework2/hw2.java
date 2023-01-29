9b4b16dba96d8c991fe2ee994943954ae062b0e7
DenysKasianov <kasyanovwork1993@gmail.com>
2023-01-29 14:59:20 -0700

    Home work2

---
+++ src/Homework2/hw2.java
@@ -0,0 +1,17 @@
+package Homework2;
+
+public class hw2 {
+    public static void main(String[] args) {
+        float pi = 3.141592653f;
+        double e = 2.7182818284590452;
+        System.out.println("pi = " + pi);
+        System.out.println("e = " + e);
+
+        String row1 = "\nмоя строка 1";
+        String row2 = "\tмоя строка 2";
+        //String row3 = "\aмоя строка 3"; Illegal escape character in string literal
+        System.out.println(row1);
+        System.out.println(row2);
+        //System.out.println(row3);
+    }
+}
